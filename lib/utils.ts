4965ee4e73b52bd80e6fb7aa82a8242cf6bc4276
krisshh-24 <sagarkrish2408@gmail.com>
2025-08-05 23:32:45 +0530

    fixed typescript bugs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,4 @@
 /* eslint-disable prefer-const */
-/* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
@@ -85,11 +84,11 @@ export function removeKeysFromQuery({
 }
 
 // DEBOUNCE
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
+export const debounce = (func: (...args: unknown[]) => void, delay: number) => {
   let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
+  return (...args: unknown[]) => {
     if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+    timeoutId = setTimeout(() => func(...args), delay);
   };
 };
 
@@ -97,7 +96,7 @@ export const debounce = (func: (...args: any[]) => void, delay: number) => {
 export type AspectRatioKey = keyof typeof aspectRatioOptions;
 export const getImageSize = (
   type: string,
-  image: any,
+  image: Record<string, unknown>,
   dimension: "width" | "height"
 ): number => {
   if (type === "fill") {
@@ -106,7 +105,7 @@ export const getImageSize = (
       1000
     );
   }
-  return image?.[dimension] || 1000;
+  return (image?.[dimension] as number) || 1000;
 };
 
 // DOWNLOAD IMAGE
@@ -131,7 +130,7 @@ export const download = (url: string, filename: string) => {
 };
 
 // DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
+export const deepMergeObjects = (obj1: Record<string, unknown>, obj2: Record<string, unknown> | null | undefined) => {
   if(obj2 === null || obj2 === undefined) {
     return obj1;
   }
@@ -146,7 +145,7 @@ export const deepMergeObjects = (obj1: any, obj2: any) => {
         obj2[key] &&
         typeof obj2[key] === "object"
       ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+        output[key] = deepMergeObjects(obj1[key] as Record<string, unknown>, obj2[key] as Record<string, unknown>);
       } else {
         output[key] = obj1[key];
       }
